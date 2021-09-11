94d93de67841f7509613fd1cb53032795654d5be
stephanxiayu <Stephan_Granzow@web.de>
2021-09-11 07:37:17 +0100

    Changes to be committed: 	modified:   lib/screens/authentication/auth.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/authentication/auth.dart
+++ lib/screens/authentication/auth.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:untitled/constants/asset_paths.dart';
+import 'package:lottie/lottie.dart';
 import 'package:untitled/controllers/appController.dart';
 import 'package:untitled/screens/authentication/widgets/bottom_text.dart';
 import 'package:untitled/screens/authentication/widgets/login.dart';
@@ -13,44 +13,49 @@ class AuthenticationScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Obx(() => SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: MediaQuery.of(context).size.width / 3),
-            Text("SG Technology", style: TextStyle(fontSize: 35),),
-            SizedBox(height: MediaQuery.of(context).size.width / 5),
-
-            Visibility(
+      body: Obx(() => SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 20,),
+               Text("SG Technology", style: TextStyle(fontSize: 35),),
+              
+              Container(child: Lottie.network("https://assets10.lottiefiles.com/packages/lf20_xlV8ZH.json")),
+             
+           
+      
+              Visibility(
+                  visible: _appController.isLoginWidgetDisplayed.value,
+                  child: LoginWidget()),
+              Visibility(
+                  visible: !_appController.isLoginWidgetDisplayed.value,
+                  child: RegistrationWidget()),
+              SizedBox(
+                height: 10,
+              ),
+              Visibility(
                 visible: _appController.isLoginWidgetDisplayed.value,
-                child: LoginWidget()),
-            Visibility(
+                child: BottomTextWidget(
+                  onTap: () {
+                    _appController.changeDIsplayedAuthWidget();
+                  },
+                  text1: "Don\'t have an account?",
+                  text2: "Create account!",
+                ),
+              ),
+              Visibility(
                 visible: !_appController.isLoginWidgetDisplayed.value,
-                child: RegistrationWidget()),
-            SizedBox(
-              height: 10,
-            ),
-            Visibility(
-              visible: _appController.isLoginWidgetDisplayed.value,
-              child: BottomTextWidget(
-                onTap: () {
-                  _appController.changeDIsplayedAuthWidget();
-                },
-                text1: "Don\'t have an account?",
-                text2: "Create account!",
+                child: BottomTextWidget(
+                  onTap: () {
+                    _appController.changeDIsplayedAuthWidget();
+                  },
+                  text1: "Already have an account?",
+                  text2: "Sign in!!",
+                ),
               ),
-            ),
-            Visibility(
-              visible: !_appController.isLoginWidgetDisplayed.value,
-              child: BottomTextWidget(
-                onTap: () {
-                  _appController.changeDIsplayedAuthWidget();
-                },
-                text1: "Already have an account?",
-                text2: "Sign in!!",
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),)
     );
